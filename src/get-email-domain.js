a763eb18fbb64c899d959069c21e0386dc99dd8c
koichi-sann <vasilyev62@gmail.com>
2022-05-01 17:11:33 +0300

    feat: add sort-by-height solution

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,7 +10,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-getEmailDomain=s=>s.match(/@[a-zA-Z0-9]+.+.+/).join('').slice(1)
+getEmailDomain=s=>s.match(/@[a-zA-Z0-9]+.+/).join('').slice(1)
 module.exports = {
   getEmailDomain
 };
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+// sortByHeight = (a) => {
+//   filtered = a.filter(i=>i>0).sort((a,b)=>a-b)
+//   count = 0
+// return sorted = a.reduce((acc, cur) => {
+//     return [...acc, (cur !== -1 ? filtered[count++] : cur)]
+//   }, [])
+// }
+sortByHeight=a=>a.reduce((q,c,i)=>c==-1?q.splice(i,0,c).concat(q):q,a.filter(i=>i>0).sort((a,b)=>a-b))
 module.exports = {
-  sortByHeight
-};
+  sortByHeight,
+}
